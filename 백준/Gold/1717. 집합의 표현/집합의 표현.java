a920fc774749d1d8c465d484f1f7da448b0ce983
jongkyuh <114567080+jongkyuh@users.noreply.github.com>
2024-06-25 21:34:34 +0900

    [Gold V] Title: 집합의 표현, Time: 2412 ms, Memory: 227408 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -2,52 +2,46 @@ import java.util.*;
 
 public class Main{
     
-    static int[] arr;
-    
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String[] str = sc.nextLine().split(" ");
-        int n = Integer.parseInt(str[0]);
-        
-        arr = new int[n+1];
-        
-        for(int i = 0; i < n + 1 ;i++){
-            arr[i] = i;
-        }
-        
-        for(int i =0; i < Integer.parseInt(str[1]); i++){
-            String[] str2 = sc.nextLine().split(" ");
-            
-            if(str2[0].equals("0")){
-                union(Integer.parseInt(str2[1]),Integer.parseInt(str2[2]));
-            }else if(str2[0].equals("1")){                
-                Boolean bl = isSame(Integer.parseInt(str2[1]),Integer.parseInt(str2[2]));
-                if(bl == true){
-                    System.out.println("YES");
-                }else{
-                    System.out.println("NO");
-                }
-            }
-        }
-        
-        
-    }
+    static int[] parent;
     
     static int find(int x){
-        if(arr[x] == x){
+        if(parent[x] == x){
             return x;
         }
-        return arr[x] = find(arr[x]);
+        return parent[x] = find(parent[x]);
     }
     
     static void union(int x, int y){
         int root1 = find(x);
         int root2 = find(y);
-        arr[root2] = root1;
+        parent[root2] = root1;      
     }
     
-    static boolean isSame(int x, int y){
-       return find(x) == find(y);
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String[] str1 = sc.nextLine().split(" ");
+        int n = Integer.parseInt(str1[0]);
+        int m = Integer.parseInt(str1[1]);
+        
+        parent = new int[n+1];
+        for(int i = 0; i < n+1;i++){
+            parent[i] = i;
+        }
+        
+        for(int i = 0;i<m;i++){
+            String[] str2 = sc.nextLine().split(" ");
+            int edge1 = Integer.parseInt(str2[1]);
+            int edge2 = Integer.parseInt(str2[2]);
+            if(str2[0].equals("0")){
+                union(edge1,edge2);
+            }else{
+                if(find(edge1) == find(edge2)){
+                    System.out.println("YES");
+                }else{
+                   System.out.println("NO"); 
+                }
+            }
+        }
+        
     }
-    
 }
