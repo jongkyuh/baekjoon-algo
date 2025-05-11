d27077312a9a3ec11883a8f800ed0e440a6a5434
jongkyuh <114567080+jongkyuh@users.noreply.github.com>
2025-05-11 17:31:41 +0900

    [Gold IV] Title: 최단경로, Time: 1748 ms, Memory: 328088 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,70 +1,73 @@
 import java.util.*;
 
 public class Main{
+    
     static class Node{
-        int end;
-        int cost;
+        int idx;    // 노드인덱스
+        int cost;    // 가중치
         
-        public Node(int end, int cost){
-            this.end = end;
+        public Node(int idx, int cost){
+            this.idx = idx;
             this.cost = cost;
-        }
+        }     
     }
     
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);    // Scanner
+
         String[] str = sc.nextLine().split(" ");
+        int n = Integer.parseInt(str[0]);    // 노드 수
+        int m = Integer.parseInt(str[1]);    // 간선 수
+        int start = Integer.parseInt(sc.nextLine());    // 시작 노드
         
-        int n = Integer.parseInt(str[0]);
-        int e = Integer.parseInt(str[1]);
-        
-        int start = Integer.parseInt(sc.nextLine());
         List<List<Node>> list = new ArrayList<>();
         for(int i = 0; i <= n; i++){
             list.add(new ArrayList<>());
         }
-        for(int i = 0; i < e; i++){
+        
+        for(int i = 0; i < m; i++){
             String[] str2 = sc.nextLine().split(" ");
-            int n1 = Integer.parseInt(str2[0]);
-            int n2 = Integer.parseInt(str2[1]);
-            int nc = Integer.parseInt(str2[2]);
-            list.get(n1).add(new Node(n2,nc));
+            int n1 = Integer.parseInt(str2[0]);    // 출발 인덱스
+            int n2 = Integer.parseInt(str2[1]);    // 도착 인덱스
+            int n3 = Integer.parseInt(str2[2]);    // 가중치
+            
+            list.get(n1).add(new Node(n2, n3));
         }
         
-        
-        int[] distance = new int[n+1];
-        for(int i = 0; i <= n; i++){
-            distance[i] = 1000000;
+        int[] distance = new int[n+1];    // 시작 노드로 부터의 거리 배열  
+        for(int i = 0; i < n+1; i++){
+            distance[i] = 10000000;
         }
         
-        PriorityQueue<Node> pq = new PriorityQueue<>((n1,n2) -> n1.cost - n2.cost);
-       
-        pq.add(new Node(start,0));
-        distance[start] = 0;
+       // Deque<Node> q = new ArrayDeque<>();
+        PriorityQueue<Node> q = new PriorityQueue<>((pq1, pq2) -> pq1.cost - pq2.cost);
+        distance[start] = 0;    // 시작에서 시작지점은 거리가 0이다.
+        q.add(new Node(start, 0));
         
-        while(!pq.isEmpty()){
-            Node pollNode = pq.poll();
-            int endV = pollNode.end;
-            int costs = pollNode.cost;
+        while(!q.isEmpty()){
+            Node pollNode = q.poll();
+            if(distance[pollNode.idx] < pollNode.cost) continue;
             
-            if(distance[endV] < costs) continue;
-            
-            for(Node getNode : list.get(endV)){
-                int nextV = getNode.end;
-                int nextCost = getNode.cost;
-                if(distance[endV] + nextCost < distance[nextV]){
-                    distance[nextV] = distance[endV] + nextCost;
-                    pq.add(new Node(nextV, distance[nextV]));
+            for(int i = 0; i < list.get(pollNode.idx).size(); i++){
+                Node newNode = list.get(pollNode.idx).get(i);
+                if(distance[newNode.idx] > distance[pollNode.idx] + newNode.cost){
+                    distance[newNode.idx]  = distance[pollNode.idx] + newNode.cost;
+                    q.add(new Node(newNode.idx, distance[newNode.idx]));
                 }
-            }
+            }           
+            
         }
-        for(int i = 1; i <= n; i++){
-            if(distance[i] < 1000000){
+        
+        for(int i = 1; i < n+1; i++){
+            if(distance[i] < 10000000){
                 System.out.println(distance[i]);
             }else{
-                System.out.println("INF");
+               System.out.println("INF"); 
             }
-               
+            
         }
+        
+           
+        
     }
 }
