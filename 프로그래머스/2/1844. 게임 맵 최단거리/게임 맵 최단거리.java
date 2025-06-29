1f98399301682f8baf862d5548d865f07e9bfb09
jongkyuh <114567080+jongkyuh@users.noreply.github.com>
2025-06-29 20:06:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.41 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    static int min = 100000000;    
+    
     static class Node{
         int x;
         int y;
@@ -10,40 +10,36 @@ class Solution {
             this.y = y;
         }
     }
-    
     public int solution(int[][] maps) {
         int answer = 0;
-        int[][] distance= new int[maps.length][maps[0].length];
-        
-        int[] addX = new int[]{-1, 0, 1, 0};
-        int[] addY = new int[]{0, 1, 0, -1 };
-        
+        int[][] visit = new int[maps.length][maps[0].length];
         Queue<Node> q = new ArrayDeque<>();
+        
+        // x와y의 이동방향 배열             
+        int[] dx = new int[]{-1, 0, 1, 0};
+        int[] dy = new int[]{0, 1, 0, -1};
+        
         q.add(new Node(0,0));
-        distance[0][0] = 1;
+        visit[0][0] = 1;
+
         while(!q.isEmpty()){
-            Node getNode = q.poll();
-            int curX = getNode.x;
-            int curY = getNode.y;
+            Node cNode = q.poll();  // 꺼낸 노드
+            int cx = cNode.x;
+            int cy = cNode.y;
             
             for(int i = 0; i < 4; i++){
-                if(curX + addX[i] >= 0 && 
-                   curX + addX[i] <= maps.length - 1 &&
-                   curY + addY[i] >= 0 &&
-                   curY + addY[i] <= maps[0].length - 1){
-                    if(distance[curX + addX[i]][curY + addY[i]] == 0 && maps[curX + addX[i]][curY + addY[i]] == 1){
-                        q.add(new Node(curX + addX[i], curY + addY[i]));
-                        distance[curX + addX[i]][curY + addY[i]] = distance[curX][curY] + 1;
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+                if(nx >= 0 && nx < maps.length && ny >= 0 && ny < maps[0].length){
+                    if(maps[nx][ny] == 1 && visit[nx][ny] == 0){
+                        q.add(new Node(nx, ny));
+                        visit[nx][ny] = visit[cx][cy] + 1;
                     }
                 }
             }
         }
         
-        if(distance[maps.length - 1][maps[0].length - 1] == 0){
-            answer =  -1;
-        }else{
-            answer =  distance[maps.length - 1][maps[0].length - 1];
-        }
-        return answer;
+        int result = visit[visit.length - 1][visit[0].length - 1];
+        return result != 0 ? result : -1;
     }
 }
