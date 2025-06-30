b8fc99ef6bd58edc4248cf3c7a9bb64f872a74c7
jongkyuh <114567080+jongkyuh@users.noreply.github.com>
2025-06-30 15:40:42 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 172 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -1,14 +1,15 @@
 import java.util.*;
 
 public class Main{
+    static long dfs(int n){
+        if(n == 1 || n == 0) return 1;
+        return n* dfs(n-1);
+    }
     public static void main(String[] args){
         long answer = 1L;
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = 0; i <=n; i++){
-            if(i == 0 || i == 1)continue;
-            answer *= i;
-        }
+        answer = dfs(n);
         System.out.println(answer);
     } 
 }
