fd2395c6e2bce5143ace8f4c8854f26876c146f4
jongkyuh <114567080+jongkyuh@users.noreply.github.com>
2025-07-04 10:14:29 +0900

    [Silver III] Title: 바이러스, Time: 196 ms, Memory: 18156 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,37 +1,45 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main{
-    public static int n;
-    public static int m;
-    static int[][] map = new int[101][101];
-    static int[] visit = new int[101];
-    static int count = 0;
-    
-    
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
+       Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int E = sc.nextInt();
         
-        sc.nextLine();
+        boolean[] visit = new boolean[N+1];
         
-        for(int i = 0;i<m;i++){
-            String st = sc.nextLine();
-            String[] sp = st.split(" ");
-            int a = Integer.parseInt(sp[0]);
-            int b = Integer.parseInt(sp[1]);
-            map[a][b] = map[b][a] = 1;           
+        List<Integer>[] edges = new ArrayList[N+1];
+        for(int i = 0; i < edges.length; i++){
+            edges[i] = new ArrayList<>();
         }
-        dfs(1);
-        System.out.println(count);
-    }
-    static void dfs(int start){
-        visit[start] = 1;
-        for(int i = 1;i<n+1;i++){
-            if(map[start][i] == 1 && visit[i] == 0){
-                count++;
-                dfs(i);
+        
+        for(int i = 0; i < E; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            edges[a].add(b);
+            edges[b].add(a);
+        }
+        
+        Queue<Integer> q = new ArrayDeque<>();
+        
+        q.add(1);
+        visit[1] = true;
+        
+        while(!q.isEmpty()){
+            int pollNum = q.poll();
+            for(int k : edges[pollNum]){
+                if(!visit[k]){
+                    q.add(k);
+                    visit[k] = true;
+                }
             }
         }
+        
+        int count = 0;
+        for(boolean b : visit){
+            if(b)count++;
+        }
+        System.out.println(count - 1);
+        
     }
 }
