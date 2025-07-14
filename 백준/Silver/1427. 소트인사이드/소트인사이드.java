4d251b9b1d38e2b81e95dcc38ef16d1b01936bdb
jongkyuh <114567080+jongkyuh@users.noreply.github.com>
2025-07-14 10:41:53 +0900

    [Silver V] Title: 소트인사이드, Time: 168 ms, Memory: 17884 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,18 +1,25 @@
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-
-        char[] arr = s.toCharArray();
-        Arrays.sort(arr);
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = arr.length - 1; i >= 0; i--) {
-            sb.append(arr[i]);
+        int[] arr = new int[s.length()];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = s.charAt(i) - '0';
         }
-
+        
+        arr = Arrays.stream(arr)
+                    .boxed()
+                    .sorted((e1,e2) -> e2.compareTo(e1))
+                    .mapToInt(Integer::intValue)
+                    .toArray();
+        StringBuilder sb = new StringBuilder();
+        
+        for(int k : arr){
+            sb.append(k);
+        }
+        
         System.out.println(sb.toString());
     }
-}
+} 
