ae4e4b51e8eecfc711eaf23320211fe216fc13b4
jongkyuh <114567080+jongkyuh@users.noreply.github.com>
2025-07-17 23:57:58 +0900

    [Silver IV] Title: 문자열 집합, Time: 752 ms, Memory: 99900 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -3,22 +3,24 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String[] str = sc.nextLine().split(" ");
-        int n = Integer.parseInt(str[0]);
-        int m = Integer.parseInt(str[1]);
-        String[] strList = new String[n];
+        int count = 0;
+        String[] str1 = sc.nextLine().split(" ");
+        int n = Integer.parseInt(str1[0]);
+        int m = Integer.parseInt(str1[1]);
+        String[] str = new String[n];    // str : 확인할 문자열 
         Set<String> set = new HashSet<>();
         
         for(int i = 0; i < n; i++){
-         set.add(sc.nextLine());
+            String sefe = sc.nextLine();
+            set.add(sefe);
         }
         
-        int count = 0;
-        for(int i = 0 ; i < m; i++){
+        for(int j = 0; j < m; j++){
             String s = sc.nextLine();
             if(set.contains(s)){
                 count++;
-            } 
+              
+            }
         }
         
         System.out.println(count);
