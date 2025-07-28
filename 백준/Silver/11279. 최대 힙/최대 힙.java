6ac6fede8e3bcba60c659214ed1490b6e357645f
jongkyuh <114567080+jongkyuh@users.noreply.github.com>
2025-07-28 17:12:58 +0900

    [Silver II] Title: 최대 힙, Time: 1332 ms, Memory: 110680 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -2,18 +2,20 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args){
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         Scanner sc = new Scanner(System.in);
+        PriorityQueue pq = new PriorityQueue<>(Collections.reverseOrder());
         int N = sc.nextInt();
         for(int i = 0; i < N; i++){
-            int k = sc.nextInt();
-            if(k == 0){
-                if(pq.size() == 0) System.out.println(0);
-                else System.out.println(pq.poll());
+            int input = sc.nextInt();
+            if(input == 0){
+                if(pq.size() == 0){
+                    System.out.println(0);
+                }else{
+                    System.out.println(pq.poll());
+                }
             }else{
-                pq.add(k);
+                pq.add(input);
             }
         }
-        
     }
 }
