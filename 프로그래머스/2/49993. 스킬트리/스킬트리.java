e21dbfff93e6e8da050c39dec046af555a7bd964
jongkyuh <114567080+jongkyuh@users.noreply.github.com>
2025-10-22 15:19:39 +0900

    [level 2] Title: 스킬트리, Time: 9.38 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,12 +1,11 @@
+import java.util.*;
+
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
         for(String s : skill_trees){
-            StringBuilder sb = new StringBuilder();
-            for(char c : s.toCharArray()){
-                if(skill.indexOf(c) != -1) sb.append(c);
-            }
-            if(skill.startsWith(sb.toString())) answer++;
+            s = s.replaceAll("[^" + skill + "]", "");
+            if(skill.startsWith(s)) answer++;
         }
         return answer;
     }
