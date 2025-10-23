04c661790e8611154ffc134bf118f1b52f9cdf77
jongkyuh <114567080+jongkyuh@users.noreply.github.com>
2025-10-23 15:12:57 +0900

    [level 2] Title: H-Index, Time: 2.04 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,16 +2,19 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        Arrays.sort(citations);
-        int n = citations.length;
-        
-        for (int i = 0; i < n; i++) {
-            int h = n - i; // 현재 인덱스 이후에 h편 이상 존재
-            if (citations[i] >= h) {
-                return h;
+        int answer = 0;
+        List<Integer> list = new ArrayList<>();
+        for(int i : citations){
+            list.add(i);
+        }
+        Collections.sort(list);
+        for(int i = 0; i < list.size(); i++){
+            int h = list.size() - i;
+            if(list.get(i) >= h){
+                answer = h;
+                break;
             }
         }
-        
-        return 0;
+        return answer;
     }
 }
