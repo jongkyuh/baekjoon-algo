25d2ab935dacce869eb233c6aa61db0fc536854e
jongkyuh <114567080+jongkyuh@users.noreply.github.com>
2025-10-26 21:33:58 +0900

    [level 2] Title: 더 맵게, Time: 1596.02 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -3,35 +3,27 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        // List<Integer> list = new ArrayList<>();
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        for(int n : scoville){
-            pq.add(n);
-        }
-        
+        for(int n : scoville) pq.add(n);
         
         while(true){
-            if(pq.size() == 1) break;
-            
-            boolean isBreak = true;
-            for(int get : pq){
-                if(get < K){
-                    isBreak = false;
+            if(pq.size() == 1){
+                if(pq.poll() < K){
+                    return -1;
+                }else{
                     break;
                 }
             }
-            
-            if(isBreak) break;
-            else{                
-                
-                int num1 = pq.poll();
-                int num2 = pq.poll();
-                pq.add(num1 + num2 * 2);
+            int first = pq.poll();  // 가장 작은 맵기
+            if(first < K){
+                int second = pq.poll();
+                pq.add(first + (second * 2));
                 answer++;
+            }else{
+                break;
             }
         }
-        if(pq.poll() < K) return -1;
+        
         return answer;
     }
 }
