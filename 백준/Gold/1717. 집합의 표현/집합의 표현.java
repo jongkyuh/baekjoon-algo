ae7db89cfd61d75ac02a10551105ed33bcba5726
jongkyuh <114567080+jongkyuh@users.noreply.github.com>
2026-03-03 15:52:00 +0900

    [Gold V] Title: 집합의 표현, Time: 1772 ms, Memory: 261036 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,44 +1,41 @@
 import java.util.*;
 
 public class Main{
-    
     static int[] parent;
-    
     static int find(int x){
-        if(parent[x] == x){
-            return x;
+        if(parent[x] != x){
+            return parent[x] = find(parent[x]);
         }
-        return parent[x] = find(parent[x]);
+        return parent[x];
     }
     
     static void union(int x, int y){
-        int root1 = find(x);
-        int root2 = find(y);
-        parent[root2] = root1;      
+        if(find(x) != find(y)){
+            int a = find(x);
+            int b = find(y);
+            parent[b] = a;
+        }
     }
-    
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String[] str1 = sc.nextLine().split(" ");
-        int n = Integer.parseInt(str1[0]);
-        int m = Integer.parseInt(str1[1]);
-        
-        parent = new int[n+1];
-        for(int i = 0; i < n+1;i++){
+        int N = sc.nextInt();
+        int M = sc.nextInt(); // M만큼 반복
+        parent = new int[N+1];
+        // 1. 부모배열 초기화(처음엔 자기자신으로)
+        for(int i = 0; i <= N; i++){
             parent[i] = i;
         }
-        
-        for(int i = 0;i<m;i++){
-            String[] str2 = sc.nextLine().split(" ");
-            int edge1 = Integer.parseInt(str2[1]);
-            int edge2 = Integer.parseInt(str2[2]);
-            if(str2[0].equals("0")){
-                union(edge1,edge2);
+        for(int i = 0; i < M; i++){
+            int check = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(check == 0){
+                union(a,b);
             }else{
-                if(find(edge1) == find(edge2)){
+                if(find(a) == find(b)){
                     System.out.println("YES");
                 }else{
-                   System.out.println("NO"); 
+                    System.out.println("NO");
                 }
             }
         }
